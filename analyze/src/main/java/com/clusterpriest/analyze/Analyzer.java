becd41e1ec93b3519d875434717b559769914656
Ashish Singh <asingh@cloudera.com>
2016-04-28 13:15:00 -0700

    Add Kafka reader

--- analyze/src/main/java/com/clusterpriest/analyze/Analyzer.java
+++ analyze/src/main/java/com/clusterpriest/analyze/Analyzer.java
@@ -1,5 +1,3 @@
-package com.clusterpriest.analyze;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
  * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
@@ -12,9 +10,91 @@ package com.clusterpriest.analyze;
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
+package com.clusterpriest.analyze;
+
+import kafka.serializer.StringDecoder;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Analyzer {
+    private static final Logger logger = LoggerFactory.getLogger(Analyzer.class);
+
+    private static final String KAFKA_BROKERS = "kafka.brokers";
+    private static final String KAFKA_TOPICS = "kafka.topics";
+
+    private static final String SPARK_APP_NAME = "spark.app.name";
+    private static final String SPARK_MASTER = "spark.master";
+    private static final String SPARK_BATCH_DURATION = "spark.batch.duration";
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String confFile;
+        if (args.length != 1) {
+            logger.warn("A config file is expected as argument. Using default file, conf/analyzer.conf");
+            confFile = "conf/analyzer.conf";
+        } else {
+            confFile = args[0];
+        }
+
+        logger.info("Starting analysis");
+
+        Context context;
+        try {
+            context = new Context(confFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Create context
+        SparkConf sparkConf = new SparkConf().setAppName(context.getString(SPARK_APP_NAME));
+        sparkConf.setMaster(context.getString(SPARK_MASTER));
+        JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkConf,
+                Durations.seconds(Integer.parseInt(context.getString(SPARK_BATCH_DURATION))));
+
+        HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(context.getString(KAFKA_TOPICS)
+                .split(",")));
+        HashMap<String, String> kafkaParams = new HashMap<String, String>();
+        kafkaParams.put("metadata.broker.list", context.getString(KAFKA_BROKERS));
+        //kafkaParams.put("auto.offset.reset", "smallest");
+
+
+        // Create direct kafka stream with brokers and topics
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                javaStreamingContext,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
+
+        // Get the json, split them into words, count the words and print
+        JavaDStream<String> json = messages.map(new Function<Tuple2<String, String>, String>() {
+            @Override
+            public String call(Tuple2<String, String> tuple2) {
+                return tuple2._2();
+            }
+        });
+        json.print();
+
+        // Start the computation
+        javaStreamingContext.start();
+        javaStreamingContext.awaitTermination();
+
+        logger.info("Done with sentiment analysis");
     }
 }
