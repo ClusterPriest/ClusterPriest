fcad8c07522a10bb4307d0e86fd94c1cb3d163d1
Ashish Singh <asingh@cloudera.com>
2016-04-28 23:23:56 -0700

    Hack for json mistakes again

--- analyze/src/main/java/com/clusterpriest/analyze/Parser/LogStringParser.java
+++ analyze/src/main/java/com/clusterpriest/analyze/Parser/LogStringParser.java
@@ -13,6 +13,7 @@
 package com.clusterpriest.analyze.Parser;
 
 import java.text.ParseException;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,11 +36,13 @@ public class LogStringParser {
     public LogData parse(String logString) throws ParseException {
         Pattern logPattern = Pattern.compile("(\\S+ \\S+) +(\\S+) +\\s*(\\S+)\\s*\\: +(.*)");
         Matcher matcher = logPattern.matcher(logString);
+        
         boolean found = matcher.find();
         LOG.info("Log string: " + logString + ", matcher found: " + found);
         if (found) {
             LOG.info("Start index: " + matcher.start());
             LOG.info(" End index: " + matcher.end() + " ");
+            LOG.info(" Group count : " + matcher.groupCount());
             LOG.info("group--" + matcher.group());
             return new LogData(matcher.group(0), matcher.group(1), matcher.group(2), matcher.group(3));
         }
